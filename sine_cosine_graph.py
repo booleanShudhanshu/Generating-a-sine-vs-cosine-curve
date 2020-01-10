4259a662cafd41f7ad4dfce76639357c1c269906
codeTillUDie <shudhanshuraj17@gmail.com>
2020-01-10 16:26:27 +0530

    Add files via upload

---
+++ sine_cosine_graph.py
@@ -0,0 +1,14 @@
+# Author- Shudhanshu Raj
+
+# For this project, you will have a generate a sine vs cosine curve.
+# You will need to use the numpy library to access the sine and cosine functions.
+# You will also need to use the matplotlib library to draw the curve.
+# To make this more difficult, make the graph go from -360° to 360°,
+# with there being a 180° difference between each point on the x-axis.
+
+from matplotlib import pyplot
+import numpy as np 
+
+x=[-360,-180,0,180,360]
+sine_y=[0,0,0,0,0,0]
+cosine_y=[1,1,1,1,1]
